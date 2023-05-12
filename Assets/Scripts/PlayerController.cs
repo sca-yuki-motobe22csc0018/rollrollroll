d40cb7b6692c4e92852e78bb172a97764c84e4ff
yuki <sdt22csc0018@edu.sca.ac.jp>
2023-05-12 19:46:45 +0900

    last update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20;
+    public Text winText;
 
     private Rigidbody rb;
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
         rb=GetComponent<Rigidbody>();
+        score = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -37,6 +43,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            score = score + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        // すべての収集アイテムを獲得した場合
+        if (score >= 32)
+        {
+            // リザルトの表示を更新
+            winText.text = "You Win!";
         }
     }
 }
